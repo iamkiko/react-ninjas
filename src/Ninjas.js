734808c2964b61f10355e61857c3250b767e94ee
iamkiko <constantinides.ch@gmail.com>
2019-04-27 16:49:40 +0200

    added conditional output + submission form

--- src/Ninjas.js
+++ src/Ninjas.js
@@ -2,14 +2,14 @@ import React, { Component } from 'react';
 
 const Ninjas = ({ninjas}) => {
     const ninjaList = ninjas.map(ninja => {
-        return (
+        return ninja.age > 20 ? (
             <div className="ninja" key={ ninja.id }>
-                <div>Name: { ninja.name }</div>
-                <div>Age: { ninja.age }</div>
-                <div>Belt: { ninja.belt }</div>
-            </div>
-        )
-    })
+            <div>Name: { ninja.name }</div>
+            <div>Age: { ninja.age }</div>
+            <div>Belt: { ninja.belt }</div>
+        </div>
+        ) : null;        
+    });
     return (
         <div className="ninja-list">
             { ninjaList }
